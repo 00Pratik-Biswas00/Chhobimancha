49fd14412bce203a7f9d64fdad1bcf3005e143f7
sattwikeeg100 <sattwikeeghosh95@gmail.com>
2024-10-25 09:57:46 +0530

    Chatbot added

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -4,6 +4,7 @@ import Slider from "react-slick";
 import axiosInstance from "../../config/axiosInstance";
 import HomeCardSlider from "./homeCardSlider.jsx";
 import SkeletonLoaderHeader from "./Skeletons/SkeletonLoaderHeader";
+import { FaCommentDots, FaTimes } from "react-icons/fa"; // Chatbot icon and close icon
 import "react-loading-skeleton/dist/skeleton.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
@@ -56,6 +57,7 @@ const Home = () => {
   const [shows, setShows] = useState([]);
   const [moviesLoading, setMoviesLoading] = useState(true);
   const [showsLoading, setShowsLoading] = useState(true);
+  const [isChatbotOpen, setIsChatbotOpen] = useState(false); // Chatbot state
 
   const GetAllMovies = async () => {
     try {
@@ -130,26 +132,25 @@ const Home = () => {
     GetAllMovies();
     GetAllShows();
   }, []);
-/*   useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, []); */
+
+  const toggleChatbot = () => {
+    setIsChatbotOpen(!isChatbotOpen);
+  };
 
   return (
     <div
       id="Home"
-      className=" w-screen h-fit flex flex-col items-center bg-background1 text-primary_text font-semibold font-montserrat"
-    >
+      className="w-screen h-fit flex flex-col items-center bg-background1 text-primary_text font-semibold font-montserrat">
+      {/* Movie Sliders */}
       {moviesLoading ? (
         <SkeletonLoaderHeader />
       ) : (
         <header className="h-full w-full my-5 min-h-[60vw] md:min-h-[30vw]">
           <div id="headerMovies" className="w-full h-full">
-            <Slider {...settings} className="  mx-6 md:mx-14 object-center">
+            <Slider {...settings} className="mx-6 md:mx-14 object-center">
               {recentMovies.map((movie) => (
                 <Link key={movie.slug} to={`explore/movies/${movie.slug}`}>
-                  <div className=" w-[100%] min-w-[60%] min-h-[35vw] md:min-h-[30vw] ">
+                  <div className="w-[100%] min-w-[60%] min-h-[35vw] md:min-h-[30vw]">
                     {(
                       <div className="w-[95%] sm:w-full h-[50vw] md:h-[30vw]" />
                     ) && (
@@ -166,7 +167,9 @@ const Home = () => {
           </div>
         </header>
       )}
-      <div className="w-screen h-full flex justify-center items-center min-h-5 pb-10 ">
+
+      {/* Latest, Popular Movies and Shows */}
+      <div className="w-screen h-full flex justify-center items-center min-h-5 pb-10">
         <div className="w-full h-full">
           <HomeCardSlider
             elements={latestMovies}
@@ -188,6 +191,34 @@ const Home = () => {
           />
         </div>
       </div>
+
+      {/* Floating Chatbot Button */}
+      <div
+        className="fixed bottom-6 right-6 z-50 cursor-pointer"
+        onClick={toggleChatbot}>
+        <div
+          className="bg-highlight text-primary_text w-16 h-16 rounded-full flex items-center justify-center shadow-lg hover:bg-highlight_hover transition duration-300"
+          title={isChatbotOpen ? "Close Chatbot" : "Open Chatbot"}>
+          {isChatbotOpen ? (
+            <FaTimes className="text-3xl" />
+          ) : (
+            <FaCommentDots className="text-3xl" />
+          )}
+        </div>
+      </div>
+
+      {/* Chatbot Iframe Modal */}
+      {isChatbotOpen && (
+        <div className="fixed bottom-20 right-6 w-80 h-96 md:w-[400px] md:h-[600px] bg-black shadow-lg border rounded-lg z-40 transition-transform duration-500">
+          <iframe
+            src="https://www.chatbase.co/chatbot-iframe/tfI7e7vG9V_CBNIf-gYjk"
+            width="100%"
+            height="100%"
+            frameBorder="0"
+            title="Chatbot"
+            className="rounded-lg"></iframe>
+        </div>
+      )}
     </div>
   );
 };
